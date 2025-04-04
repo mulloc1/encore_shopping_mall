be40d601b0b6b01c0e5126185fd802b6c0f98330
mulloc1 <skybgk1@gmail.com>
2025-04-04 22:09:10 +0900

    <feat>[providers] cart item_list modified

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,29 @@
+import 'package:encore_shopping_mall/models/item.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CartNotifier extends Notifier<List<Item>> {
+  @override
+  List<Item> build() {
+    return [];
+  }
+
+  void addItem(Item item) {
+    state = [...state, item];
+  }
+
+  void removeItem(Item item) {
+    state = state.where((i) => i != item).toList();
+  }
+
+  void updateItem(Item oldItem, Item newItem) {
+    state = state.map((item) => item == oldItem ? newItem : item).toList();
+  }
+
+  void clearCart() {
+    state = [];
+  }
+}
+
+final cartProvider = NotifierProvider<CartNotifier, List<Item>>(() {
+  return CartNotifier();
+});
--- lib/providers/item_list_provider.dart
+++ lib/providers/item_list_provider.dart
@@ -1,6 +1,28 @@
 import 'package:encore_shopping_mall/models/item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final itemListProvider = StateProvider<List<Item>>((ref) {
-  return [];
+class ItemListNotifier extends StateNotifier<List<Item>> {
+  ItemListNotifier() : super([]);
+
+  void addItem(Item item) {
+    state = [...state, item];
+  }
+
+  void removeItem(String id) {
+    state = state.where((item) => item.id != id).toList();
+  }
+
+  void updateItem(Item updatedItem) {
+    state = state.map((item) => 
+      item.id == updatedItem.id ? updatedItem : item
+    ).toList();
+  }
+
+  void clearItems() {
+    state = [];
+  }
+}
+
+final itemListProvider = StateNotifierProvider<ItemListNotifier, List<Item>>((ref) {
+  return ItemListNotifier();
 });
